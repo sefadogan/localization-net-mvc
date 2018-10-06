0f490700182c80a1b311aeffc0e9765c4b413564
sefadogan <sefadogan7@gmail.com>
2018-10-07 01:21:50 +0300

    Language Controller added. Global.asax and some codes updated.

---
+++ LocalizationDemo/Localization/Controllers/LanguageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Localization.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public ActionResult ChangeLanguage(string language)
+        {
+            if (language != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+
+            HttpCookie cookie = new HttpCookie("language")
+            {
+                Value = language
+            };
+
+            Response.Cookies.Add(cookie);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
